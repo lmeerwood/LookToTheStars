aa370cf37752203fe0b6cdc62d27ff135e48c19b
Leonard Meerwood <lenny@comitplatforms.com>
2016-09-30 13:12:50 +1000

    initial commit

---
+++ app/src/main/java/com/meerwood/leonard/looktothestars/SelectCelestialObjectActivity.java
@@ -0,0 +1,13 @@
+package com.meerwood.leonard.looktothestars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectCelestialObjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_celestial_object);
+    }
+}
